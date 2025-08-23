a18c53f5a538c587265057164fb72c42e7b4a3a0
DilanDFrya <dilandilman.work@gmail.com>
2025-08-23 19:00:33 +0300

    NavBar: HomePage 100% DashBoard 100% BoardPage 0%

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/components/navbar";
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradent-to-br from-blue-50 via-white to-purple-60">
-    <Navbar/>
+      <Navbar />
     </div>
   );
 }
